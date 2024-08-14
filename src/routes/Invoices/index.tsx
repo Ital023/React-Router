19b9906221f739747c9c7d4e0cd16731d05b6a34
Ital023 <contadoitalo911@gmail.com>
2024-08-14 16:43:50 -0300

    feat: Active links

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,30 +1,31 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { getInvoices } from "../../data";
+import "./styles.css"
 
 export default function Invoices() {
   const invoices = getInvoices();
-  
+
   return (
     <>
-    <div style={{ display: "flex" }}>
-      <nav
-        style={{
-          borderRight: "solid 1px",
-          padding: "1rem",
-        }}
-      >
-        {invoices.map((invoice) => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </Link>
-        ))}
-      </nav>
-      <Outlet />
-    </div>
+      <div style={{ display: "flex" }}>
+        <nav
+          style={{
+            borderRight: "solid 1px",
+            padding: "1rem",
+          }}
+        >
+          {invoices.map((invoice) => (
+            <NavLink
+              className={({ isActive }) => (isActive ? "d-block nav-red" : "d-block nav-blue")}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
+        </nav>
+        <Outlet />
+      </div>
     </>
   );
 }
